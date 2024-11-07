6c963c8a0a10b0b98049ded28666ac647fff3355
Tirth <tvp590@uregina.ca>
2024-11-06 20:08:40 -0600

    feat: Lab3 completed

---
+++ Lab3/customTreeTopo.py
@@ -0,0 +1,45 @@
+from mininet.topo import Topo
+
+class CustomTreeTopo( Topo ):
+    def build(self):
+        h1 = self.addHost( 'h1' )
+        h2 = self.addHost( 'h2' )
+        h3 = self.addHost( 'h3' )
+        h4 = self.addHost( 'h4' )
+        h5 = self.addHost( 'h5' )
+        h6 = self.addHost( 'h6' )
+        h7 = self.addHost( 'h7' )
+        h8 = self.addHost( 'h8' )
+
+        s1 = self.addSwitch( 's1' )
+        s2 = self.addSwitch( 's2' )
+        s3 = self.addSwitch( 's3' )
+        s4 = self.addSwitch( 's4' )
+        s5 = self.addSwitch( 's5' )
+        s6 = self.addSwitch( 's6' )
+        s7 = self.addSwitch( 's7' )
+        s8 = self.addSwitch( 's8' )
+        s9 = self.addSwitch( 's9' )
+        s10 = self.addSwitch( 's10' )
+        s11 = self.addSwitch( 's11' )
+
+        self.addLink( h1, s1 )
+        self.addLink( h2, s2 )
+        self.addLink( h3, s3 )
+        self.addLink( h4, s4 )
+        self.addLink( s1, s5 )
+        self.addLink( s2, s5 )
+        self.addLink( s3, s6 )
+        self.addLink( s4, s6 )
+        self.addLink( s5, s7 )
+        self.addLink( s6, s7 )
+        self.addLink( s7, s8 )
+        self.addLink( s8, h5 )
+        self.addLink( s8, h6 )
+        self.addLink( s8, s9 )
+        self.addLink( s9, s10 )
+        self.addLink( s9, s11 )
+        self.addLink( s10, h7 )
+        self.addLink( s11, h8 )
+
+topos = { 'customTreeTopo': ( lambda: CustomTreeTopo() ) }
